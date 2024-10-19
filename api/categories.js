632c71c646bf1a5feb6be9439b59e55a7547de45
Leonardo <cleonardo191@gmail.com>
2024-10-19 10:53:17 +0100

    aa

---
+++ api/categories.js
@@ -0,0 +1,29 @@
+const admin = require("firebase-admin");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+}
+
+const db = admin.database();
+
+module.exports = async (req, res) => {
+  try {
+    const ref = db.ref("menu/categories");
+    const snapshot = await ref.once("value");
+    const data = snapshot.val();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
