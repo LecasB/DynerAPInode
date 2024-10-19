a638dd82ae42fcbd9ccb0bf0094abd6c0bc34033
Leonardo <cleonardo191@gmail.com>
2024-10-19 10:59:35 +0100

    ola

--- api/menu.js
+++ api/menu.js
@@ -18,6 +18,14 @@ if (!admin.apps.length) {
 const db = admin.database();
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
     const ref = db.ref("menu/menus");
     const snapshot = await ref.once("value");
