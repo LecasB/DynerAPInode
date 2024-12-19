aa14e3b33921b2086f5c2292559bc1d0c828d292
Leonardo Carvalho <cleonardo191@gmail.com>
2024-12-19 13:23:26 +0000

    asdjas

--- api/newaction.js
+++ api/newaction.js
@@ -35,29 +35,22 @@ module.exports = async (req, res) => {
         return res.status(400).json({ error: "Message is required" });
       }
 
-      // Reference the "menu/actions" node in the Firebase Realtime Database
       const ref = db.ref("menu/actions");
 
-      // Fetch the current actions to calculate the new ID
       const snapshot = await ref.once("value");
-      const actions = snapshot.val() || []; // Default to an empty array if no data exists
+      const actions = snapshot.val();
 
-      // Calculate the next ID
       const newId = actions.length > 0 ? actions[actions.length - 1].id + 1 : 1;
 
-      // Prepare the new data
       const newData = {
         id: newId,
         message: newMessage,
       };
 
-      // Add the new data to the array
       actions.push(newData);
 
-      // Save the updated array back to Firebase
       await ref.set(actions);
 
-      // Respond with the success message and the new data
       res.status(200).json({
         message: "Data successfully added to Firebase!",
         data: newData,
@@ -67,7 +60,6 @@ module.exports = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   } else {
-    // If method is not POST, return "Method Not Allowed"
     res.status(405).json({ error: "Method Not Allowed" });
   }
 };
