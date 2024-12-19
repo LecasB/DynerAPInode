cb12d920b00340fbad95d5a9cf532b445720d241
Leonardo Carvalho <cleonardo191@gmail.com>
2024-12-19 13:14:01 +0000

    ola

--- api/newaction.js
+++ api/newaction.js
@@ -1,20 +1,3 @@
-const admin = require("firebase-admin");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-  });
-}
-
 const db = admin.database();
 
 module.exports = async (req, res) => {
@@ -22,7 +5,6 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  // Handle CORS preflight request
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
@@ -35,7 +17,6 @@ module.exports = async (req, res) => {
         return res.status(400).json({ error: "Message is required" });
       }
 
-      // Reference the "menu/actions" node in the Firebase Realtime Database
       const ref = db.ref("menu/actions");
 
       // Fetch the last record to get the highest current ID
@@ -48,9 +29,8 @@ module.exports = async (req, res) => {
       let newId = 1;
 
       if (lastItem) {
-        // Get the last key (ID) and increment it
         const lastKey = Object.keys(lastItem)[0];
-        newId = parseInt(lastKey) + 1; // Increment the last ID by 1
+        newId = parseInt(lastItem[lastKey].id) + 1; // Increment the highest ID
       }
 
       const newData = {
@@ -58,10 +38,9 @@ module.exports = async (req, res) => {
         message: newMessage,
       };
 
-      // Push the new data to Firebase under "menu/actions"
-      await ref.push(newData);
+      // Use the custom ID as the key
+      await ref.child(newId).set(newData);
 
-      // Respond with the success message and the new data
       res.status(200).json({
         message: "Data successfully added to Firebase!",
         data: newData,
@@ -71,7 +50,6 @@ module.exports = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   } else {
-    // If method is not POST, return "Method Not Allowed"
     res.status(405).json({ error: "Method Not Allowed" });
   }
 };
