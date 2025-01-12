0207ade9316d0991279a5a2084ae0d802278632c
Leonardo <cleonardo191@gmail.com>
2025-01-12 01:20:32 +0000

    asdsadsad

--- api/newaction.js
+++ api/newaction.js
@@ -49,11 +49,24 @@ module.exports = async (req, res) => {
 
       actions.push(newData);
 
+      function formatDate(date) {
+        const day = String(date.getDate()).padStart(2, "0");
+        const month = String(date.getMonth() + 1).padStart(2, "0"); // Os meses começam em 0
+        const year = date.getFullYear();
+
+        const hours = String(date.getHours()).padStart(2, "0");
+        const minutes = String(date.getMinutes()).padStart(2, "0");
+        const seconds = String(date.getSeconds()).padStart(2, "0");
+
+        return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+      }
+
       await ref.set(actions);
 
       res.status(200).json({
         message: "Data successfully added to Firebase!",
         data: newData,
+        date: formatDate(new Date()),
       });
     } catch (error) {
       console.error(error);
